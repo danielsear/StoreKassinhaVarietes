c760d3c2cb7ce9c72afb1e9ebf8887f28205ec87
Danielsear <danielandradeseara@hotmail.com>
2022-05-29 17:53:27 -0300

    conected database mongodb

--- Backend/src/index.js
+++ Backend/src/index.js
@@ -1,5 +1,8 @@
+require('dotenv').config()
+
 const express = require('express')
 const morgan = require('morgan')
+const mongoose = require('mongoose')
 
 const app = express()
 
@@ -8,6 +11,17 @@ app.use(express.urlencoded({ extended: true }))
 app.use(morgan('dev'))
 
 app.use(require('./routes'))
+
+const uri = process.env.MONGOOSE_URL_CONECTION
+
+mongoose
+  .connect(uri, {
+    useNewUrlParser: true
+  })
+  .then(() => {
+    console.log(' Conecting mongoose ')
+  })
+  .catch(err => console.error(err))
 
 app.listen(3333, () => {
   console.log(' Server online!')

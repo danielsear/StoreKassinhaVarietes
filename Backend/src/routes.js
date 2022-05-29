6eb827a094cb95dd106fa2848f4b46cb555a41a3
Danielsear <danielandradeseara@hotmail.com>
2022-05-29 18:07:21 -0300

    created image route

--- Backend/src/routes.js
+++ Backend/src/routes.js
@@ -2,9 +2,15 @@ const routes = require('express').Router()
 const multer = require('multer')
 const multerConfig = require('./config/multer')
 
-routes.post('/image', multer(multerConfig).single('file'), (req, res) => {
-  console.log(req.file)
-  res.json({ message: 'Hello World!' })
+routes.post('/image', multer(multerConfig).single('file'), async (req, res) => {
+  const { originalname: name, size, key } = req.file
+  const image = await Image.create({
+    name,
+    size,
+    key,
+    url: ''
+  })
+  return res.json(image)
 })
 
 module.exports = routes

df05ef5fb8c78df6cd30d8ca28daafcc4238655f
Danielsear <danielandradeseara@hotmail.com>
2022-05-29 18:34:31 -0300

    Saving image in mongoose and putting its url

--- Backend/src/index.js
+++ Backend/src/index.js
@@ -3,12 +3,19 @@ require('dotenv').config()
 const express = require('express')
 const morgan = require('morgan')
 const mongoose = require('mongoose')
+const path = require('path')
+const cors = require('cors')
 
 const app = express()
 
+app.use(cors())
 app.use(express.json())
 app.use(express.urlencoded({ extended: true }))
 app.use(morgan('dev'))
+app.use(
+  '/files',
+  express.static(path.resolve(__dirname, '..', 'tmp', 'uploads'))
+)
 
 app.use(require('./routes'))
 

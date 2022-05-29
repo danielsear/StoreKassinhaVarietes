90d84bd98c583258fc952fb29213a5e7be6cc788
Danielsear <danielandradeseara@hotmail.com>
2022-05-29 18:26:24 -0300

    List images

--- Backend/src/routes.js
+++ Backend/src/routes.js
@@ -2,6 +2,15 @@ const routes = require('express').Router()
 const multer = require('multer')
 const multerConfig = require('./config/multer')
 
+const Image = require('./models/Image')
+
+//IMAGES
+
+routes.get('/images', async (req, res) => {
+  const image = await Image.find()
+  return res.json(image)
+})
+
 routes.post('/image', multer(multerConfig).single('file'), async (req, res) => {
   const { originalname: name, size, key } = req.file
   const image = await Image.create({

04374901cbd566c467bc2fd0633bfd8e93fc1ef3
Danielsear <danielandradeseara@hotmail.com>
2022-06-14 23:04:30 -0300

    Show image in Create User

--- Frontend/src/Componets/FormCreateUser/index.tsx
+++ Frontend/src/Componets/FormCreateUser/index.tsx
@@ -11,6 +11,8 @@ import { CreateUser } from '../../services/User'
 function FormCreateUser() {
   const navegate = useNavigate()
   const [messageError, setMessageError] = useState('')
+  const [image, setImage] = useState<File>()
+
   const [user, setUser] = useState({
     userId: '',
     name: '',
@@ -64,39 +66,72 @@ function FormCreateUser() {
     <div className="form_container">
       <div className="form_header">Kassinha Variedades</div>
       <h2>Create Login:</h2>
-      <form className="form_Cadastro" onSubmit={handleSubmit}>
-        <div>
-          <label htmlFor="name">Nome:</label>
-          <input
-            type="text"
-            name="name"
-            placeholder="Digite seu nome"
-            onChange={handleUser}
-            required
-          />
+      <div className="form_quadro">
+        <form className="form_Cadastro" onSubmit={handleSubmit}>
+          <div>
+            <label htmlFor="name">Nome:</label>
+            <input
+              type="text"
+              name="name"
+              placeholder="Digite seu nome"
+              onChange={handleUser}
+              required
+            />
+          </div>
+          <div>
+            <label htmlFor="email">Email:</label>
+            <input
+              type="email"
+              name="email"
+              placeholder="Digite seu email"
+              onChange={handleUser}
+              required
+            />
+          </div>
+          <div>
+            <label htmlFor="password">Senha:</label>
+            <input
+              type="password"
+              name="password"
+              placeholder="Digite sua senha"
+              onChange={handleUser}
+              required
+            />
+          </div>
+          <div>
+            <label htmlFor="file">Carregar imagem:</label>
+            <input
+              type="file"
+              name="image"
+              onChange={e => {
+                const data = e.target.files
+                if (data) {
+                  setImage(data[0])
+                }
+              }}
+              required
+            />
+          </div>
+          <button type="submit">Cadastrar</button>
+        </form>
+        <div className="show-image">
+          {image ? (
+            <img
+              src={URL.createObjectURL(image)}
+              alt="Image"
+              width="300"
+              height="300"
+            />
+          ) : (
+            <img
+              src="/usuario-default.png"
+              alt="Image"
+              width="300"
+              height="300"
+            />
+          )}
         </div>
-        <div>
-          <label htmlFor="email">Email:</label>
-          <input
-            type="email"
-            name="email"
-            placeholder="Digite seu email"
-            onChange={handleUser}
-            required
-          />
-        </div>
-        <div>
-          <label htmlFor="password">Senha:</label>
-          <input
-            type="password"
-            name="password"
-            placeholder="Digite sua senha"
-            onChange={handleUser}
-            required
-          />
-        </div>
-        <button type="submit">Cadastrar</button>
-      </form>
+      </div>
     </div>
   )
 }
